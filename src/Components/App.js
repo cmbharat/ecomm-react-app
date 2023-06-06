444d86a9a7ce1e888ec96b3af03e911498f84e05
cmbharat <bharatcm70@gmail.com>
2023-06-06 23:54:12 +0530

    initial setup done

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import "../Styles/App.css";
+import Navbar from "./Navbar";
+import "../Styles/App.css";
+import Home from "./Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
